80202029c588d199422abb57e94bcdac0f3bd91f
aqua28 <aidyl889@gmail.com>
2016-10-21 14:34:54 -0700

    farm

--- Chick.java
+++ Chick.java
@@ -3,13 +3,21 @@ class Chick implements Animal
 {     
     private String myType;     
      private String mySound;      
-     public Chick(String type, String sound)     {         
-         myType = type;         
-         mySound = sound;     
-     }     
+     public Chick(String type, String sound2, String sound1)     {         
+         myType = type;      
+   
+         if(Math.random()>.5){
+         mySound = sound1;
+         }  
+         else{
+         mySound =  sound2;            
+            }  
+     }
+         
+
+	
      public Chick()     {         
-         myType = "unknown";         
-         mySound = "unknown";     
+         
      }      
      public String getSound(){return mySound;}     
      public String getType(){return myType;} 
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,10 +1,7 @@
 
-	public void setup() {     
-    Cow c = new Cow("cow", "moo");   
-    System.out.println(c.getType() + " goes " + c.getSound());   
-    Pig p = new Pig("pig", "moo");
-    System.out.println(p.getType() + " goes " + p.getSound());
-    Chick v = new Chick("chick", "cheep");
-    System.out.println(v.getType() + " goes " + v.getSound());
-}
+	public void setup() {   
 
+	Farm hello = new Farm();
+
+	hello.animalSounds();
+}
